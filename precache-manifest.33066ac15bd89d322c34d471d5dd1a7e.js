5e73fdcef2c8a00578ff1620503268fcc31e5ea8
unknown <7273prodigy@mail.ru>
2020-04-28 20:04:34 +0300

    Updates

---
+++ precache-manifest.33066ac15bd89d322c34d471d5dd1a7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "193f6277ef98c42cc6ac2c86210ffde7",
+    "url": "/bukschop/index.html"
+  },
+  {
+    "revision": "9a5ca359c7fdba73c9e6",
+    "url": "/bukschop/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "c0841e096319ded80f3e",
+    "url": "/bukschop/static/css/main.c5bb5416.chunk.css"
+  },
+  {
+    "revision": "9a5ca359c7fdba73c9e6",
+    "url": "/bukschop/static/js/2.bc5a483b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bukschop/static/js/2.bc5a483b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0841e096319ded80f3e",
+    "url": "/bukschop/static/js/main.77783a3c.chunk.js"
+  },
+  {
+    "revision": "22d0f8fa8d94ead4a722",
+    "url": "/bukschop/static/js/runtime-main.d70ada7a.js"
+  }
+]);
