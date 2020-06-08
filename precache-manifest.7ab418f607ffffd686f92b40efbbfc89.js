29b6daf83349e804c819b882470c81c9a08fcbf0
dmitryE <11464632+thirtythreeboas@users.noreply.github.com>
2020-06-09 00:16:02 +0300

    Update precache-manifest.7ab418f607ffffd686f92b40efbbfc89.js

--- precache-manifest.7ab418f607ffffd686f92b40efbbfc89.js
+++ precache-manifest.7ab418f607ffffd686f92b40efbbfc89.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "5b54bd0abb1d59115b497de7ba6713ba",
-    "url": "/bukschop/index.html"
+    "url": "/bookstore/index.html"
   },
   {
     "revision": "9a5ca359c7fdba73c9e6",
-    "url": "/bukschop/static/css/2.89f067aa.chunk.css"
+    "url": "/bookstore/static/css/2.89f067aa.chunk.css"
   },
   {
     "revision": "8fef1c37433e2efd3ab2",
-    "url": "/bukschop/static/css/main.41eb3ddb.chunk.css"
+    "url": "/bookstore/static/css/main.41eb3ddb.chunk.css"
   },
   {
     "revision": "9a5ca359c7fdba73c9e6",
-    "url": "/bukschop/static/js/2.bc5a483b.chunk.js"
+    "url": "/bookstore/static/js/2.bc5a483b.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/bukschop/static/js/2.bc5a483b.chunk.js.LICENSE.txt"
+    "url": "/bookstore/static/js/2.bc5a483b.chunk.js.LICENSE.txt"
   },
   {
     "revision": "8fef1c37433e2efd3ab2",
-    "url": "/bukschop/static/js/main.8b3a3721.chunk.js"
+    "url": "/bookstore/static/js/main.8b3a3721.chunk.js"
   },
   {
     "revision": "22d0f8fa8d94ead4a722",
-    "url": "/bukschop/static/js/runtime-main.d70ada7a.js"
+    "url": "/bookstore/static/js/runtime-main.d70ada7a.js"
   }
 ]);
